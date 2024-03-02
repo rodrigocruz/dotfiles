7f1cfe59c7c750ad2f6af625011e09f0f37bb2ad
Rodrigo Cruz <dev@rodrigocruz.pt>
2024-03-02 18:40:11 +0000

    Added new mappings

--- nvim/lua/user/mappings.lua
+++ nvim/lua/user/mappings.lua
@@ -1,36 +1,37 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = ""
+vim.keymap.set("i", "jk", "<Esc>")
 
 require("user/wk-mappings")
 
 for i = 1, 6 do
-  local lhs = "<Leader>" .. i
-  local rhs = i .. "<C-W>w"
-  vim.keymap.set('n', lhs, rhs, { desc = "Move to window " .. i })
+    local lhs = "<Leader>" .. i
+    local rhs = i .. "<C-W>w"
+    vim.keymap.set("n", lhs, rhs, { desc = "Move to window " .. i })
 end
 
 -- Reselect visual selection after indenting.
-vim.keymap.set('v', '<', '<gv')
-vim.keymap.set('v', '>', '>gv')
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
 
 -- Maintain the cursor position when yanking a visual selection.
 -- http://ddrscott.github.io/blog/2016/yank-without-jank/
-vim.keymap.set('v', 'y', 'myy`y')
-vim.keymap.set('v', 'Y', 'myY`y')
+vim.keymap.set("v", "y", "myy`y")
+vim.keymap.set("v", "Y", "myY`y")
 
 -- When text is wrapped, move by terminal rows, not lines, unless a count is provided.
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
 
 -- Paste replace visual selection without copying it.
-vim.keymap.set('v', 'p', '"_dP')
+vim.keymap.set("v", "p", '"_dP')
 
 -- Easy insertion of a trailing ; or , from insert mode.
-vim.keymap.set('i', ';;', '<Esc>A;<Esc>')
-vim.keymap.set('i', ',,', '<Esc>A,<Esc>')
+vim.keymap.set("i", ";;", "<Esc>A;<Esc>")
+vim.keymap.set("i", ",,", "<Esc>A,<Esc>")
 
 -- Resize with arrows.
-vim.keymap.set('n', '<C-Up>', ':resize +2<CR>')
-vim.keymap.set('n', '<C-Down>', ':resize -2<CR>')
-vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>')
-vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>')
+vim.keymap.set("n", "<C-Up>", ":resize +2<CR>")
+vim.keymap.set("n", "<C-Down>", ":resize -2<CR>")
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>")
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>")
--- nvim/lua/user/wk-mappings.lua
+++ nvim/lua/user/wk-mappings.lua
@@ -2,55 +2,64 @@ local wk = require("which-key")
 local dap = require("dap")
 
 wk.register({
-  ["0"] = { ":Neotree filesystem focus right<CR>", "Focus Neotree" },
-  ["<tab>"] = { ":bprevious<CR>", "Previous buffer"},
-  b = {
-    name = "Buffers",
-    b = { ":FzfLua buffers<CR>", "List buffers" },
-    C = { ":bufdo bdelete<CR>", "Close All Open Buffers"},
-  },
-  c = {
-    name = "Code",
-    a = { vim.lsp.buf.code_action, "Code Actions"},
-  },
-  d = {
-    name = "Debuggind",
-    b = { dap.toggle_breakpoint, "Set Breakpoint" },
-    c = { dap.continue, "Continue" },
-    x = { dap.close, "Close"}
-  },
-  e = {
-    name = "Errors",
-    l = { ":TroubleToggle document_diagnostics<CR>", "List Errors"},
-    n = { vim.diagnostic.goto_next, "Next Error"},
-    p = { vim.diagnostic.goto_prev, "Previous Error"},
-    q = { ":TroubleToggle quickfix<CR>", "Quickfix"}
-  },
-  f = {
-    name = "File",
-    r = { ":FzfLua oldfiles<CR>", "Recent Files"},
-    t = { ":Neotree filesystem toggle right<CR>", "Open Neotree" },
-    f = { ":FzfLua files<CR>", "Find files" },
-  },
-  g = {
-    name = "Git",
-    s = { ":Neogit<CR>", "Git Status"},
-  },
-  h = {
-    name = "Help",
-    h = { ":FzfLua help_tags<CR>", "Help tags" },
-  },
-  s = {
-    name = "Search",
-    c = { ":nohlsearch<CR>", "Clear Search Highlight"},
-    p = { ":FzfLua live_grep<CR>", "Search in project" },
-    l = { ":FzfLua resume<CR>", "Resume last search"},
-    s = { ":FzfLua lgrep_curbuf<CR>", "FuzZy search current buffer"},
-  },
-  w = {
-    name = "Windows",
-    v = { ":vsplit<CR>", "Split window vertically"},
-    d = { "<C-w>q", "Close window"},
-    o = { ":only<CR>", "Close other wndows"},
-  },
+	["0"] = { ":Neotree filesystem focus right<CR>", "Focus Neotree" },
+	["<tab>"] = { ":bprevious<CR>", "Previous buffer" },
+	b = {
+		name = "Buffers",
+		b = { ":FzfLua buffers<CR>", "List buffers" },
+		C = { ":bufdo bdelete<CR>", "Close All Open Buffers" },
+	},
+	c = {
+		name = "Code",
+		a = { vim.lsp.buf.code_action, "Code Actions" },
+		f = { vim.lsp.buf.format, "Code Format" },
+	},
+	d = {
+		name = "Debuggind",
+		b = { dap.toggle_breakpoint, "Set Breakpoint" },
+		c = { dap.continue, "Continue" },
+		x = { dap.close, "Close" },
+	},
+	e = {
+		name = "Errors",
+		l = { ":TroubleToggle document_diagnostics<CR>", "List Errors" },
+		n = { vim.diagnostic.goto_next, "Next Error" },
+		p = { vim.diagnostic.goto_prev, "Previous Error" },
+		q = { ":TroubleToggle quickfix<CR>", "Quickfix" },
+	},
+	f = {
+		name = "File",
+		r = { ":FzfLua oldfiles<CR>", "Recent Files" },
+		t = { ":Neotree filesystem toggle right<CR>", "Open Neotree" },
+		f = { ":FzfLua files<CR>", "Find files" },
+	},
+	g = {
+		name = "Git",
+		s = { ":Neogit<CR>", "Git Status" },
+	},
+	h = {
+		name = "Help",
+		h = { ":FzfLua help_tags<CR>", "Help tags" },
+	},
+	s = {
+		name = "Search",
+		c = { ":nohlsearch<CR>", "Clear Search Highlight" },
+		p = { ":FzfLua live_grep<CR>", "Search in project" },
+		l = { ":FzfLua resume<CR>", "Resume last search" },
+		s = { ":FzfLua lgrep_curbuf<CR>", "FuzZy search current buffer" },
+	},
+    t = {
+        name = "Test",
+        n = { ":TestNearest<CR>", "Test Nearest"},
+        f = { ":TestFile<CR>", "Test File"},
+        s = { ":TestSuite<CR>", "Test Suite"},
+        l = { ":TestLast<CR>", "Test Last"},
+        v = { ":TestVisit<CR>", "Test Visit"},
+    },
+	w = {
+		name = "Windows",
+		v = { ":vsplit<CR>", "Split window vertically" },
+		d = { "<C-w>q", "Close window" },
+		o = { ":only<CR>", "Close other wndows" },
+	},
 }, { prefix = "<leader>" })
