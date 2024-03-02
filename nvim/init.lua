5c0dd309d58757cb1773921cada77129705317f3
Rodrigo Cruz <dev@rodrigocruz.pt>
2024-03-02 18:39:24 +0000

    Changed plugins folder in init

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,5 @@
+require("user/plugins")
 require("user/options")
-require("plugins")
 require("user/whichkey")
 require("user/mappings")
 
-vim.cmd([[
-  augroup FileTypeOverrides
-    autocmd!
-    autocmd TermOpen * setlocal nospell
-  augroup END
-]])
-
-vim.keymap.set("i","jk","<Esc>")
