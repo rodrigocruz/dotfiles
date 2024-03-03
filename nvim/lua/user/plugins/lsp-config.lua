baf2ed21f9684d50905cf9da61992a9e32e5716c
Rodrigo Cruz <dev@rodrigocruz.pt>
2024-03-03 23:29:38 +0000

    Improved completions and formatting

--- nvim/lua/user/plugins/lsp-config.lua
+++ nvim/lua/user/plugins/lsp-config.lua
@@ -1,15 +1,15 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
                     "lua_ls",
                     "intelephense",
                     "tailwindcss",
@@ -19,90 +19,118 @@ return {
                     "jsonls",
                     "tsserver",
                     "yamlls",
+                    "antlersls",
+                    "volar",
                 },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
+            local lspconfig = require("lspconfig")
 
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.intelephense.setup({
-				capabilities = capabilities,
-				filetypes = { "php", "blade" },
-				settings = {
-					intelephense = {
-						filetypes = { "php", "blade" },
-						files = {
-							associations = { "*.php", "*.blade.php" }, -- Associating .blade.php files as well
-							maxSize = 5000000,
-						},
-					},
-				},
-			})
+            lspconfig.tsserver.setup({
+                capabilities = capabilities,
+            })
+            -- lspconfig.volar.setup({
+            --     capabilities = capabilities,
+            --     on_attach = function(client, bufnr)
+            --         client.server_capabilities.documentFormattingProvider = false
+            --         client.server_capabilities.documentRangeFormattingProvider = false
+            --         -- if client.server_capabilities.inlayHintProvider then
+            --         --   vim.lsp.buf.inlay_hint(bufnr, true)
+            --         -- end
+            --     end,
+            --     -- Enable "Take Over Mode" where volar will provide all JS/TS LSP services
+            --     -- This drastically improves the responsiveness of diagnostic updates on change
+            --     filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+            -- })
+            lspconfig.html.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.intelephense.setup({
+                capabilities = capabilities,
+                filetypes = { "php", "blade" },
+                settings = {
+                    intelephense = {
+                        filetypes = { "php", "blade" },
+                        files = {
+                            associations = { "*.php", "*.blade.php" }, -- Associating .blade.php files as well
+                            maxSize = 5000000,
+                        },
+                    },
+                },
+            })
             lspconfig.tailwindcss.setup({
-                capabilities = capabilities
+                capabilities = capabilities,
             })
             lspconfig.cssls.setup({
-                capabilities = capabilities
+                capabilities = capabilities,
             })
             lspconfig.emmet_ls.setup({
-                capabilities = capabilities
+                capabilities = capabilities,
             })
             lspconfig.jsonls.setup({
-                capabilities = capabilities
+                capabilities = capabilities,
             })
             lspconfig.yamlls.setup({
-                capabilities = capabilities
+                capabilities = capabilities,
             })
 
+            lspconfig.lua_ls.setup({})
 
-			lspconfig.lua_ls.setup({})
+            -- vim.keymap.set('n', '<space>z', vim.diagnostic.open_float)
+            --vim.keymap.set('n', '<space>en', vim.diagnostic.goto_next)
+            --vim.keymap.set('n', '<space>ep', vim.diagnostic.goto_prev)
+            --vim.keymap.set('n', '<space>el', vim.diagnostic.setloclist)
+            vim.api.nvim_create_autocmd("LspAttach", {
+                group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+                callback = function(ev)
+                    -- Enable completion triggered by <c-x><c-o>
+                    -- vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+                    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-			-- vim.keymap.set('n', '<space>z', vim.diagnostic.open_float)
-			--vim.keymap.set('n', '<space>en', vim.diagnostic.goto_next)
-			--vim.keymap.set('n', '<space>ep', vim.diagnostic.goto_prev)
-			--vim.keymap.set('n', '<space>el', vim.diagnostic.setloclist)
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-				callback = function(ev)
-					-- Enable completion triggered by <c-x><c-o>
-					-- vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-                    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+                    -- Buffer local mappings.
+                    -- See `:help vim.lsp.*` for documentation on any of the below functions
+                    local opts = { buffer = ev.buf }
+                    -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                    --vim.keymap.set('n', '<space>cd', vim.lsp.buf.definition, opts)
+                    -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                    -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                    -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+                    -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+                    -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+                    -- vim.keymap.set('n', '<space>wl', function()
+                    --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                    -- end, opts)
+                    -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+                    -- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+                    --vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+                    -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                    -- vim.keymap.set('n', '<space>f', function()
+                    -- vim.lsp.buf.format { async = true } end, opts)
+                end,
+            })
 
-					-- Buffer local mappings.
-					-- See `:help vim.lsp.*` for documentation on any of the below functions
-					local opts = { buffer = ev.buf }
-					-- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-					--vim.keymap.set('n', '<space>cd', vim.lsp.buf.definition, opts)
-					-- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-					-- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-					-- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-					-- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-					-- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-					-- vim.keymap.set('n', '<space>wl', function()
-					--   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-					-- end, opts)
-					-- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-					-- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-					--vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-					-- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-					-- vim.keymap.set('n', '<space>f', function()
-					-- vim.lsp.buf.format { async = true } end, opts)
-				end,
-			})
-		end,
-	},
+            -- Diagnostic configuration
+            vim.diagnostic.config({
+                virtual_text = false,
+                float = {
+                    source = true,
+                }
+            })
+
+            -- Sign configuration
+            vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
+            vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn' })
+            vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
+            vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
+        end,
+    },
 }
--- nvim/lua/user/plugins/none-ls.lua
+++ nvim/lua/user/plugins/none-ls.lua
@@ -1,15 +1,43 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.blade_formatter,
-			},
-		})
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.prettier,
+                null_ls.builtins.formatting.blade_formatter,
+                -- null_ls.builtins.diagnostics.eslint_d.with({
+                --     condition = function(utils)
+                --         return utils.root_has_file({ ".eslintrc.js" })
+                --     end,
+                -- }),
+                -- null_ls.builtins.diagnostics.phpstan, -- TODO: Only if config file
+                null_ls.builtins.diagnostics.trail_space.with({ disabled_filetypes = { "NvimTree" } }),
+                -- null_ls.builtins.formatting.eslint_d.with({
+                --     condition = function(utils)
+                --         return utils.root_has_file({ ".eslintrc.js", ".eslintrc.json" })
+                --     end,
+                -- }),
+                null_ls.builtins.formatting.pint.with({
+                    condition = function(utils)
+                        return utils.root_has_file({ "vendor/bin/pint" })
+                    end,
+                }),
+                null_ls.builtins.formatting.prettier.with({
+                    condition = function(utils)
+                        return utils.root_has_file({
+                            ".prettierrc",
+                            ".prettierrc.json",
+                            ".prettierrc.yml",
+                            ".prettierrc.js",
+                            "prettier.config.js",
+                        })
+                    end,
+                }),
+            },
+        })
 
-		-- vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
-	end,
+        -- vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
+    end,
 }
