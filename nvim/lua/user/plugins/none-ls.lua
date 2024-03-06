6f62531730d344cbf604b2f900a93428736be3c4
Rodrigo Cruz <dev@rodrigocruz.pt>
2024-03-06 12:34:14 +0000

    Fixed eslint

--- nvim/lua/user/plugins/none-ls.lua
+++ nvim/lua/user/plugins/none-ls.lua
@@ -1,5 +1,8 @@
 return {
     "nvimtools/none-ls.nvim",
+    dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+    },
     config = function()
         local null_ls = require("null-ls")
         null_ls.setup({
@@ -7,6 +10,8 @@ return {
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.formatting.prettier,
                 null_ls.builtins.formatting.blade_formatter,
+                require("none-ls.diagnostics.eslint_d"),
+                require("none-ls.formatting.eslint_d"),
                 -- null_ls.builtins.diagnostics.eslint_d.with({
                 --     condition = function(utils)
                 --         return utils.root_has_file({ ".eslintrc.js" })
