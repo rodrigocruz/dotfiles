2d662fa1f9e8137a248b2a443de6c9d773eab8e3
Rodrigo Cruz <dev@rodrigocruz.pt>
2024-03-06 12:15:12 +0000

    Disable navic

--- nvim/lua/user/plugins/barbecue.lua
+++ nvim/lua/user/plugins/barbecue.lua
@@ -1,11 +1,16 @@
 return {
-  'utilyre/barbecue.nvim',
-  event = 'BufRead',
-  dependencies = {
-    "SmiteshP/nvim-navic",
-    "nvim-tree/nvim-web-devicons",
-  },
-  opts = {
-    theme = 'tokyonight',
-  },
+    "utilyre/barbecue.nvim",
+    config = function ()
+        require("barbecue").setup({
+          attach_navic = false, -- prevent barbecue from automatically attaching nvim-navic
+        })
+    end,
+    event = "BufRead",
+    dependencies = {
+        "SmiteshP/nvim-navic",
+        "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+        theme = "tokyonight",
+    },
 }
