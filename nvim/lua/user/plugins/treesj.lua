099427a87560b4f1a9b5173bcc02d30665df7d22
Rodrigo Cruz <dev@rodrigocruz.pt>
2024-03-07 10:19:54 +0000

    Activated treesj keybinds

--- nvim/lua/user/plugins/treesj.lua
+++ nvim/lua/user/plugins/treesj.lua
@@ -2,8 +2,8 @@ return {
   'Wansmer/treesj',
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
   keys = {
-    -- { 'gJ', function() require('treesj').join() end },
-    -- { 'gS', function() require('treesj').split() end },
+    { 'gJ', function() require('treesj').join() end },
+    { 'gS', function() require('treesj').split() end },
   },
   opts = {
     use_default_keymaps = false,
