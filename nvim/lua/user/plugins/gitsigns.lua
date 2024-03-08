46db97fe199c097a1915a45b4fe867217cd819d0
Rodrigo Cruz <dev@rodrigocruz.pt>
2024-03-08 12:33:55 +0000

    Hide git blame inline

--- nvim/lua/user/plugins/gitsigns.lua
+++ nvim/lua/user/plugins/gitsigns.lua
@@ -2,18 +2,18 @@ return {
   'lewis6991/gitsigns.nvim',
   lazy = false,
   keys = {
-    { ']h', ':Gitsigns next_hunk<CR>'},
-    { '[h', ':Gitsigns prev_hunk<CR>'},
-    { 'gs', ':Gitsigns stage_hunk<CR>'},
-    { 'gS', ':Gitsigns undo_stage_hunk<CR>'},
-    { 'gp', ':Gitsigns preview_hunk<CR>'},
-    { 'gb', ':Gitsigns blame_line<CR>'},
+    -- { ']h', ':Gitsigns next_hunk<CR>'},
+    -- { '[h', ':Gitsigns prev_hunk<CR>'},
+    -- { 'gs', ':Gitsigns stage_hunk<CR>'},
+    -- { 'gS', ':Gitsigns undo_stage_hunk<CR>'},
+    -- { 'gp', ':Gitsigns preview_hunk<CR>'},
+    -- { 'gb', ':Gitsigns blame_line<CR>'},
   },
   opts = {
     preview_config = {
       border = { '', '', '', ' ' },
     },
-    current_line_blame = true,
+    current_line_blame = false,
     current_line_blame_opts = {
       virt_text = true,
       virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
