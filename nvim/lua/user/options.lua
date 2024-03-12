b4cdd82b21b789176ea975f74842a0c5b287b2ba
Rodrigo Cruz <dev@rodrigocruz.pt>
2024-03-12 21:32:36 +0000

    Added config for neovide

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -15,29 +15,33 @@ vim.opt.wrap = false
 vim.opt.breakindent = true -- maintain indent when wrapping indented lines
 vim.opt.linebreak = true -- wrap at word boundaries
 vim.opt.list = true -- enable the below listchars
-vim.opt.listchars = { tab = '▸ ', trail = '·' }
-vim.opt.fillchars:append({ eob = ' ' }) -- remove the ~ from end of buffer
-vim.opt.mouse = 'a' -- enable mouse for all modes
+vim.opt.listchars = { tab = "▸ ", trail = "·" }
+vim.opt.fillchars:append({ eob = " " }) -- remove the ~ from end of buffer
+vim.opt.mouse = "a" -- enable mouse for all modes
 vim.opt.mousemoveevent = true -- Allow hovering in bufferline
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
-vim.opt.clipboard = 'unnamedplus' -- Use Linux system clipboard
+vim.opt.clipboard = "unnamedplus" -- Use Linux system clipboard
 vim.opt.confirm = true -- ask for confirmation instead of erroring
 vim.opt.undofile = true -- persistent undo
 vim.opt.backup = true -- automatically save a backup file
-vim.opt.backupdir:remove('.') -- keep backups out of the current directory
+vim.opt.backupdir:remove(".") -- keep backups out of the current directory
 vim.opt.shortmess:append({ I = true }) -- disable the splash screen
-vim.opt.wildmode = 'longest:full,full' -- complete the longest common match, and allow tabbing the results to fully complete them
-vim.opt.completeopt = 'menuone,longest,preview'
-vim.opt.signcolumn = 'yes:2'
+vim.opt.wildmode = "longest:full,full" -- complete the longest common match, and allow tabbing the results to fully complete them
+vim.opt.completeopt = "menuone,longest,preview"
+vim.opt.signcolumn = "yes:2"
 vim.opt.showmode = false
 vim.opt.updatetime = 4001 -- Set updatime to 1ms longer than the default to prevent polyglot from changing it
 vim.opt.redrawtime = 10000 -- Allow more time for loading syntax on large files
 vim.opt.exrc = true
 vim.opt.secure = true
-vim.opt.titlestring = '%f // nvim'
+vim.opt.titlestring = "%f // nvim"
 vim.opt.hlsearch = true
 
 vim.opt.exrc = true
+
+if vim.g.neovide then
+	vim.o.guifont = "JetBrainsMono Nerd Font:h17"
+end
