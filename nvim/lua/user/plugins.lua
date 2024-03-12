c9e61deeaf8c4355f1e570c2ff8167b7c4dc9200
Rodrigo Cruz <dev@rodrigocruz.pt>
2024-03-12 21:32:14 +0000

    Installed vim-rooter

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,108 +1,108 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	-- Color scheme
-	-- { import = "user.plugins.tokyonight" },
+    -- Color scheme
+    -- { import = "user.plugins.tokyonight" },
 
     { import = "user.plugins.everforest" },
 
     -- { import = "user.plugins.catppuccin" },
 
-	{ import = "user.plugins.alpha" },
+    { import = "user.plugins.alpha" },
 
-	{ import = "user.plugins.autopairs" },
+    { import = "user.plugins.autopairs" },
 
-	{ import = "user.plugins.completions" },
+    { import = "user.plugins.completions" },
 
-	{ import = "user.plugins.debugging" },
+    { import = "user.plugins.debugging" },
 
-	{ import = "user.plugins.fzf-lua" },
+    { import = "user.plugins.fzf-lua" },
 
-	{ import = "user.plugins.lsp-config" },
+    { import = "user.plugins.lsp-config" },
 
-	{ import = "user.plugins.lualine" },
+    { import = "user.plugins.lualine" },
 
-	{ import = "user.plugins.neogit" },
+    { import = "user.plugins.neogit" },
 
-	{ import = "user.plugins.neotree" },
+    { import = "user.plugins.neotree" },
 
-	{ import = "user.plugins.none-ls" },
+    { import = "user.plugins.none-ls" },
 
-	{ import = "user.plugins.telescope" },
+    { import = "user.plugins.telescope" },
 
-	{ import = "user.plugins.tree-sitter" },
+    { import = "user.plugins.tree-sitter" },
 
-	{ import = "user.plugins.trouble" },
+    { import = "user.plugins.trouble" },
 
-	{ import = "user.plugins.which-key" },
+    { import = "user.plugins.which-key" },
 
-	-- Commenting support.
-	{ import = "user.plugins.vim-commentary" },
+    -- Commenting support.
+    { import = "user.plugins.vim-commentary" },
 
-	-- Add, change, and delete surrounding text.
-	{ "tpope/vim-surround" },
+    -- Add, change, and delete surrounding text.
+    { "tpope/vim-surround" },
 
-	-- Allow plugins to enable repeating of commands.
-	{ "tpope/vim-repeat" },
+    -- Allow plugins to enable repeating of commands.
+    { "tpope/vim-repeat" },
 
-	-- Navigate seamlessly between Vim windows and Tmux panes.
-	{ "christoomey/vim-tmux-navigator" },
+    -- Navigate seamlessly between Vim windows and Tmux panes.
+    { "christoomey/vim-tmux-navigator" },
 
-	-- Enable * searching with visually selected text.
-	{ "nelstrom/vim-visual-star-search" },
+    -- Enable * searching with visually selected text.
+    { "nelstrom/vim-visual-star-search" },
 
-	-- Text objects for HTML attributes.
-	{ "whatyouhide/vim-textobj-xmlattr", dependencies = "kana/vim-textobj-user" },
+    -- Text objects for HTML attributes.
+    { "whatyouhide/vim-textobj-xmlattr",       dependencies = "kana/vim-textobj-user" },
 
-	-- Add smooth scrolling to avoid jarring jumps
-	-- { "karb94/neoscroll.nvim", config = true },
+    -- Add smooth scrolling to avoid jarring jumps
+    -- { "karb94/neoscroll.nvim", config = true },
 
-	-- Automatically fix indentation when pasting code.
-	{ import = "user.plugins.vim-pasta" },
+    -- Automatically fix indentation when pasting code.
+    { import = "user.plugins.vim-pasta" },
 
-	-- Display buffers as tabs.
-	{ import = "user.plugins.bufferline" },
+    -- Display buffers as tabs.
+    { import = "user.plugins.bufferline" },
 
-	-- Git integration.
-	{ import = "user.plugins.gitsigns" },
+    -- Git integration.
+    { import = "user.plugins.gitsigns" },
 
-	--- Floating terminal.
-	-- { import = "user.plugins.floaterm" },
+    --- Floating terminal.
+    -- { import = "user.plugins.floaterm" },
 
-	-- Testing helper
-	{ import = "user.plugins.vim-test" },
+    -- Testing helper
+    { import = "user.plugins.vim-test" },
 
-	-- All closing buffers without closing the split window.
-	{ import = "user.plugins.bufdelete" },
+    -- All closing buffers without closing the split window.
+    { import = "user.plugins.bufdelete" },
 
-	-- Split arrays and methods onto multiple lines, or join them back up.
-	{ import = "user.plugins.treesj" },
+    -- Split arrays and methods onto multiple lines, or join them back up.
+    { import = "user.plugins.treesj" },
 
-	-- Show file and LSP context in a bar at the top of the screen.
-	-- { import = "user.plugins.barbecue" },
+    -- Show file and LSP context in a bar at the top of the screen.
+    -- { import = "user.plugins.barbecue" },
 
-	-- Colorize Hex Codes
-	{ import = "user.plugins.colorizer" },
+    -- Colorize Hex Codes
+    { import = "user.plugins.colorizer" },
 
-	-- Display indentation lines.
-	{ import = "user.plugins.indent-blankline" },
+    -- Display indentation lines.
+    { import = "user.plugins.indent-blankline" },
 
-	-- Project Configuration.
-	{ import = "user.plugins.projectionist" },
+    -- Project Configuration.
+    { import = "user.plugins.projectionist" },
 
-	-- PHP Refactoring Tools
-	{ import = "user.plugins.phpactor" },
+    -- PHP Refactoring Tools
+    { import = "user.plugins.phpactor" },
 
     { import = "user.plugins.nvim-bqf" },
 
@@ -122,27 +122,29 @@ require("lazy").setup({
 
     { import = "user.plugins.oil" },
 
-    { import = "user.plugins.lsp-saga"},
+    { import = "user.plugins.lsp-saga" },
 
-    { import = "user.plugins.hop"},
+    { import = "user.plugins.hop" },
 
-    { import = "user.plugins.browser-search"},
+    { import = "user.plugins.browser-search" },
 
-    { import = "user.plugins.maximize"},
+    { import = "user.plugins.maximize" },
 
-    { import = "user.plugins.rest"},
+    { import = "user.plugins.rest" },
 
     { import = "user.plugins.dash" },
 
+    -- Automatically set the working directory to the project root.
+    { import = "user.plugins.vim-rooter" },
 }, {
-	checker = {
-		enabled = true,
-		notify = false,
-	},
-	change_detection = {
-		notify = false,
-	},
-	install = {
-		colorscheme = { "tokyonight", "habamax" },
-	},
+    checker = {
+        enabled = true,
+        notify = false,
+    },
+    change_detection = {
+        notify = false,
+    },
+    install = {
+        colorscheme = { "tokyonight", "habamax" },
+    },
 })
