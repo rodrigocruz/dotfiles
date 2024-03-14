7cacd1f85d11d9bf5bd119671b27689696685ff5
Rodrigo Cruz <dev@rodrigocruz.pt>
2024-03-14 16:35:20 +0000

    Added mappings for php helpers

--- nvim/lua/user/mappings.lua
+++ nvim/lua/user/mappings.lua
@@ -39,3 +39,6 @@ vim.keymap.set("n", "=", [[<cmd>vertical resize +5<cr>]]) -- make the window big
 vim.keymap.set("n", "-", [[<cmd>vertical resize -5<cr>]]) -- make the window smaller vertically
 vim.keymap.set("n", "+", [[<cmd>horizontal resize +2<cr>]]) -- make the window bigger horizontally by pressing shift and =
 vim.keymap.set("n", "_", [[<cmd>horizontal resize -2<cr>]]) -- make the window smaller horizontally by pressing shift and -
+
+vim.keymap.set("i", "..", "->")
+vim.keymap.set("i", "//", "=>")
