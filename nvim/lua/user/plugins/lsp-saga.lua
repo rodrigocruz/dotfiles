17cca06b75b4ce6ce9ec641eb0038a206b38b09f
Rodrigo Cruz <dev@rodrigocruz.pt>
2024-03-29 12:22:29 +0000

    Increase lsp-saga window width

--- nvim/lua/user/plugins/lsp-saga.lua
+++ nvim/lua/user/plugins/lsp-saga.lua
@@ -4,6 +4,9 @@ return {
 		require("lspsaga").setup({
             lightbulb = {
                 enable = false,
+            },
+            outline = {
+                win_width = 40,
             }
         })
 	end,
