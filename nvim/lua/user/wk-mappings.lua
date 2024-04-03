c2e20b5d3ad9d07ac9cd7e2a9c11408b32def819
Rodrigo Cruz <dev@rodrigocruz.pt>
2024-04-03 01:28:38 +0100

    Fixed error

--- nvim/lua/user/wk-mappings.lua
+++ nvim/lua/user/wk-mappings.lua
@@ -6,6 +6,7 @@ wk.register({
 	["<tab>"] = { ":b#<CR>", "Previous buffer" },
 	a = {
 		name = "ChatGPT",
+        mode = { "n", "v" },
 		a = { "<cmd>ChatGPTActAs<CR>", "Act as" },
 		c = { "<cmd>ChatGPT<CR>", "ChatGPT" },
 		e = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction", mode = { "n", "v" } },
@@ -100,7 +101,7 @@ wk.register({
 	g = {
 		name = "Git",
 		b = { ":Gitsigns blame_line<CR>", "Git Blame Line" },
-		f = { ":FzfLua git_status<CR>", "Gir Files" },
+		f = { ":FzfLua git_status<CR>", "Git Files" },
 		h = { ":DiffviewFileHistory %<CR>", "Git File History" },
 		l = { ":LazyGit<CR>", "Open Lazy Git" },
 		n = { ":Gitsigns next_hunk<CR>", "Next Modification" },
@@ -207,22 +208,6 @@ wk.register({
 }, { prefix = "<leader>" })
 
 local leaderv = {
-	a = {
-		name = "ChatGPT",
-		c = { "<cmd>ChatGPT<CR>", "ChatGPT" },
-		e = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction", mode = { "n", "v" } },
-		g = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction", mode = { "n", "v" } },
-		t = { "<cmd>ChatGPTRun translate<CR>", "Translate", mode = { "n", "v" } },
-		k = { "<cmd>ChatGPTRun keywords<CR>", "Keywords", mode = { "n", "v" } },
-		d = { "<cmd>ChatGPTRun docstring<CR>", "Docstring", mode = { "n", "v" } },
-		a = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests", mode = { "n", "v" } },
-		o = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code", mode = { "n", "v" } },
-		s = { "<cmd>ChatGPTRun summarize<CR>", "Summarize", mode = { "n", "v" } },
-		f = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs", mode = { "n", "v" } },
-		x = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code", mode = { "n", "v" } },
-		r = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit", mode = { "n", "v" } },
-		l = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis", mode = { "n", "v" } },
-	},
 	c = {
 		name = "code",
 		l = {
