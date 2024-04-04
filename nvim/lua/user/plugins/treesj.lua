d7ccc05a71f7b758a7bff18a5412762805102493
Rodrigo Cruz <dev@rodrigocruz.pt>
2024-04-04 11:42:26 +0100

    Added toggle mapping

--- nvim/lua/user/plugins/treesj.lua
+++ nvim/lua/user/plugins/treesj.lua
@@ -1,11 +1,27 @@
 return {
-  'Wansmer/treesj',
-  dependencies = { 'nvim-treesitter/nvim-treesitter' },
-  keys = {
-    { 'gJ', function() require('treesj').join() end },
-    { 'gS', function() require('treesj').split() end },
-  },
-  opts = {
-    use_default_keymaps = false,
-  },
+    "Wansmer/treesj",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    keys = {
+        {
+            "gT",
+            function()
+                require("treesj").toggle()
+            end,
+        },
+        {
+            "gJ",
+            function()
+                require("treesj").join()
+            end,
+        },
+        {
+            "gS",
+            function()
+                require("treesj").split()
+            end,
+        },
+    },
+    opts = {
+        use_default_keymaps = false,
+    },
 }
