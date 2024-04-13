60fa0cc2a2566b7517bc0cec90d5770d21173ad0
Rodrigo Cruz <dev@rodrigocruz.pt>
2024-04-13 12:04:26 +0100

    Changed neogit mappings

--- nvim/lua/user/plugins/neogit.lua
+++ nvim/lua/user/plugins/neogit.lua
@@ -1,11 +1,18 @@
 return {
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim",         -- required
-    "sindrets/diffview.nvim",        -- optional - Diff integration
-
-    -- Only one of these is needed, not both.
-    "nvim-telescope/telescope.nvim", -- optional
-  },
-  config = true
+    "NeogitOrg/neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim",   -- required
+        "sindrets/diffview.nvim",  -- optional - Diff integration
+        "nvim-telescope/telescope.nvim", -- optional
+    },
+    config = function()
+        require("neogit").setup({
+            mappings = {
+                finder = {
+                    ["<C-j>"] = "Next",
+                    ["<C-k>"] = "Previous",
+                }
+            }
+        })
+    end,
 }
