e7bdb4fa30d6ec43825ac966ae6eb107129f7b35
Rodrigo Cruz <dev@rodrigocruz.pt>
2024-04-17 15:23:04 +0100

    Removed unused code

--- nvim/lua/user/plugins/gitsigns.lua
+++ nvim/lua/user/plugins/gitsigns.lua
@@ -1,14 +1,6 @@
 return {
   'lewis6991/gitsigns.nvim',
   lazy = false,
-  keys = {
-    -- { ']h', ':Gitsigns next_hunk<CR>'},
-    -- { '[h', ':Gitsigns prev_hunk<CR>'},
-    -- { 'gs', ':Gitsigns stage_hunk<CR>'},
-    -- { 'gS', ':Gitsigns undo_stage_hunk<CR>'},
-    -- { 'gp', ':Gitsigns preview_hunk<CR>'},
-    -- { 'gb', ':Gitsigns blame_line<CR>'},
-  },
   opts = {
     preview_config = {
       border = { '', '', '', ' ' },
