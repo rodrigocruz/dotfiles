8362bf2f8525119f56a79f633c9de0fb8acab2c3
Rodrigo Cruz <dev@rodrigocruz.pt>
2024-04-19 11:40:27 +0100

    Improves alpha screen

--- nvim/lua/user/plugins/alpha.lua
+++ nvim/lua/user/plugins/alpha.lua
@@ -1,9 +1,34 @@
 return {
-  {
-    'goolord/alpha-nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function ()
-      require'alpha'.setup(require'alpha.themes.startify'.config)
-    end
-  }
+	"goolord/alpha-nvim",
+	event = "VimEnter",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local alpha = require("alpha")
+		local startify = require("alpha.themes.startify")
+
+		-- Set header
+		startify.section.header.val = {
+			"                                                     ",
+			"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+			"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+			"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+			"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+			"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+			"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+			"                                                     ",
+		}
+
+        startify.opts.layout[1].val = 2
+        startify.opts.opts.margin = 100
+
+        -- disable MRU
+        startify.section.mru.val = { { type = "padding", val = 0 } }
+
+		--
+		-- Send config to alpha
+		alpha.setup(startify.config)
+
+		-- Disable folding on alpha buffer
+		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+	end,
 }
