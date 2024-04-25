6aecea7b3a9cf625812f21db95ed9b5d658b9e76
Rodrigo Cruz <dev@rodrigocruz.pt>
2024-04-25 11:53:26 +0100

    Remove ugly indent

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -14,8 +14,8 @@ vim.opt.smartcase = true
 vim.opt.wrap = false
 vim.opt.breakindent = true -- maintain indent when wrapping indented lines
 vim.opt.linebreak = true -- wrap at word boundaries
-vim.opt.list = true -- enable the below listchars
-vim.opt.listchars = { tab = "▸ ", trail = "·" }
+-- vim.opt.list = true -- enable the below listchars
+-- vim.opt.listchars = { tab = "▸ ", trail = "·" }
 vim.opt.fillchars:append({ eob = " " }) -- remove the ~ from end of buffer
 vim.opt.mouse = "a" -- enable mouse for all modes
 vim.opt.mousemoveevent = true -- Allow hovering in bufferline
