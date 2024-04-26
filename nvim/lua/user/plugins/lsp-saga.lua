d9ccee15ece4f2f2da3e586792da4f0c4c879424
Rodrigo Cruz <dev@rodrigocruz.pt>
2024-04-26 23:22:33 +0100

    Cleanup

--- nvim/lua/user/plugins/lsp-saga.lua
+++ nvim/lua/user/plugins/lsp-saga.lua
@@ -2,13 +2,13 @@ return {
 	"nvimdev/lspsaga.nvim",
 	config = function()
 		require("lspsaga").setup({
-            lightbulb = {
-                enable = false,
-            },
-            outline = {
-                win_width = 40,
-            }
-        })
+			-- lightbulb = {
+			-- 	enable = true,
+			-- },
+			-- outline = {
+			-- 	win_width = 40,
+			-- },
+		})
 	end,
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
--- nvim/lua/user/plugins/trouble.lua
+++ nvim/lua/user/plugins/trouble.lua
@@ -1,26 +1,33 @@
 return {
-    "folke/trouble.nvim",
-    branch = "dev",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-        focus = true,
-        win = {
-            size = { height = 0.3 },
-        },
-        modes = {
-            preview_float = {
-                mode = "diagnostics",
-                preview = {
-                    type = "float",
-                    relative = "editor",
-                    border = "rounded",
-                    title = "Preview",
-                    title_pos = "center",
-                    position = { 0, -2 },
-                    size = { width = 0.3, height = 0.3 },
-                    zindex = 200,
-                },
-            },
-        },
-    },
+	"folke/trouble.nvim",
+	branch = "dev",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		signs = {
+			error = "",
+			warning = "",
+			hint = "",
+			information = "",
+			other = "",
+		},
+		focus = true,
+		win = {
+			size = { height = 0.3 },
+		},
+		modes = {
+			preview_float = {
+				mode = "diagnostics",
+				preview = {
+					type = "float",
+					relative = "editor",
+					border = "rounded",
+					title = "Preview",
+					title_pos = "center",
+					position = { 0, -2 },
+					size = { width = 0.3, height = 0.3 },
+					zindex = 200,
+				},
+			},
+		},
+	},
 }
