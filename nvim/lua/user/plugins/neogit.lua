c49dfa125fdac3caf573cc46d68de3368d5879b6
Rodrigo Cruz <dev@rodrigocruz.pt>
2024-05-03 21:31:17 +0100

    Code formatting

--- nvim/lua/user/plugins/neogit.lua
+++ nvim/lua/user/plugins/neogit.lua
@@ -1,22 +1,22 @@
 return {
-    "NeogitOrg/neogit",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "sindrets/diffview.nvim",
-        "nvim-telescope/telescope.nvim",
-    },
-    config = function()
-        require("neogit").setup({
-            mappings = {
-                finder = {
-                    ["<c-j>"] = "Next",
-                    ["<c-k>"] = "Previous",
-                }
-            },
-            integrations = {
-                telescope = true,
-                diffview = true,
-            }
-        })
-    end,
+	"NeogitOrg/neogit",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"sindrets/diffview.nvim",
+		"nvim-telescope/telescope.nvim",
+	},
+	config = function()
+		require("neogit").setup({
+			mappings = {
+				finder = {
+					["<c-j>"] = "Next",
+					["<c-k>"] = "Previous",
+				},
+			},
+			integrations = {
+				telescope = true,
+				diffview = true,
+			},
+		})
+	end,
 }
