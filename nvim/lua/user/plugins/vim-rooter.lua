eaba0499182d6fafee724ef27de9890798a94d16
Rodrigo Cruz <dev@rodrigocruz.pt>
2024-05-03 21:32:05 +0100

    Silence change dir in vim-rooter

--- nvim/lua/user/plugins/vim-rooter.lua
+++ nvim/lua/user/plugins/vim-rooter.lua
@@ -1,11 +1,12 @@
 return {
-    "airblade/vim-rooter",
-    init = function()
-        -- Instead of this running every time we open a file, we'll just run it once when Vim starts.
-        vim.g.rooter_manual_only = 1
-        vim.g.rooter_patterns = { ".git", "composer.json" }
-    end,
-    config = function()
-        vim.cmd("Rooter")
-    end,
+	"airblade/vim-rooter",
+	init = function()
+		-- Instead of this running every time we open a file, we'll just run it once when Vim starts.
+		vim.g.rooter_manual_only = 1
+		vim.g.rooter_patterns = { ".git", "composer.json" }
+		vim.g.rooter_silent_chdir = 1
+	end,
+	config = function()
+		vim.cmd("Rooter")
+	end,
 }
