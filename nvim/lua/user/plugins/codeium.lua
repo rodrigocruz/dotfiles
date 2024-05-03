b9f95aa7590d7c50583f79dae96ce0451771a02d
Rodrigo Cruz <dev@rodrigocruz.pt>
2024-05-03 18:26:30 +0100

    Improved codeium keymap

--- nvim/lua/user/plugins/codeium.lua
+++ nvim/lua/user/plugins/codeium.lua
@@ -1,18 +1,18 @@
 return {
-    "Exafunction/codeium.vim",
-    event = "BufEnter",
-    config = function()
-        vim.keymap.set("i", "<C-d>", function()
-            return vim.fn["codeium#Accept"]()
-        end, { expr = true, silent = true })
-        vim.keymap.set("i", "<c-;>", function()
-            return vim.fn["codeium#CycleCompletions"](1)
-        end, { expr = true, silent = true })
-        vim.keymap.set("i", "<c-,>", function()
-            return vim.fn["codeium#CycleCompletions"](-1)
-        end, { expr = true, silent = true })
-        vim.keymap.set("i", "<c-x>", function()
-            return vim.fn["codeium#Clear"]()
-        end, { expr = true, silent = true })
-    end,
+	"Exafunction/codeium.vim",
+	event = "BufEnter",
+	config = function()
+		vim.keymap.set("i", "<C-d>", function()
+			return vim.fn["codeium#Accept"]()
+		end, { expr = true, silent = true })
+		vim.keymap.set("i", "<c-r>", function()
+			return vim.fn["codeium#CycleCompletions"](1)
+		end, { expr = true, silent = true })
+		vim.keymap.set("i", "<c-e>", function()
+			return vim.fn["codeium#CycleCompletions"](-1)
+		end, { expr = true, silent = true })
+		vim.keymap.set("i", "<c-x>", function()
+			return vim.fn["codeium#Clear"]()
+		end, { expr = true, silent = true })
+	end,
 }
