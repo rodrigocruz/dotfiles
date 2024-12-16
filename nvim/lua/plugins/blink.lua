2dc71c0280c0ec12a683bdf9c261869dd498326c
Rodrigo Cruz <labrugice@gmail.com>
2024-12-16 18:25:34 +0000

    Added options for blink

---
+++ nvim/lua/plugins/blink.lua
@@ -0,0 +1,12 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      trigger = {
+        -- When both this and show_on_trigger_character are false, will show the completion window
+        -- when the cursor comes after a trigger character after accepting an item
+        show_on_accept_on_trigger_character = false,
+      },
+    },
+  },
+}
