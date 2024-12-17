898a639321c981214b9f5c4fe35738a5f3ddae55
Rodrigo Cruz <labrugice@gmail.com>
2024-12-17 21:22:38 +0000

    Improved blink

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -6,6 +6,9 @@ return {
         -- When both this and show_on_trigger_character are false, will show the completion window
         -- when the cursor comes after a trigger character after accepting an item
         show_on_accept_on_trigger_character = false,
+        -- When both this and show_on_trigger_character are false, will show the completion window
+        -- when the cursor comes after a trigger character when entering insert mode
+        show_on_insert_on_trigger_character = false,
       },
     },
   },
