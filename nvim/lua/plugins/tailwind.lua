2013315ba28b2e65f6c89a6d536fa08114c95406
Rodrigo Cruz <dev@rodrigocruz.pt>
2026-01-21 12:31:39 +0000

    Removed tailwind hover

--- nvim/lua/plugins/tailwind.lua
+++ nvim/lua/plugins/tailwind.lua
@@ -26,14 +26,4 @@ return {
       },
     },
   },
-  {
-    "ruicsh/tailwind-hover.nvim",
-    keys = {
-      { "<leader><s-k>", "<cmd>TailwindHover<cr>", desc = "Tailwind: Hover" },
-    },
-    opts = {},
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-    },
-  },
 }
