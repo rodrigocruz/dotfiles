c43fe324de662233c73be1b0a322caafac0d9a89
Rodrigo Cruz <dev@rodrigocruz.pt>
2026-01-21 12:26:52 +0000

    Removed obsidian

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -96,12 +96,12 @@ return {
       },
     },
     smart = {},
-    image = {
-      resolve = function(path, src)
-        if require("obsidian.api").path_is_note(path) then
-          return require("obsidian.api").resolve_image_path(src)
-        end
-      end,
-    },
+    -- image = {
+    --   resolve = function(path, src)
+    --     if require("obsidian.api").path_is_note(path) then
+    --       return require("obsidian.api").resolve_image_path(src)
+    --     end
+    --   end,
+    -- },
   },
 }
