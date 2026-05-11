75fad5bc45bc096e70556cc545a0f7c2f4b7377c
Rodrigo Cruz <labrugice@gmail.com>
2026-05-11 10:11:19 +0200

    Remove duplicate php config

--- nvim/lua/plugins/php.lua
+++ nvim/lua/plugins/php.lua
@@ -1,13 +1,5 @@
 return {
   {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        php = { "pint", "php_cs_fixer" },
-      },
-    },
-  },
-  {
     -- Remove phpcs linter.
     "mfussenegger/nvim-lint",
     optional = true,
