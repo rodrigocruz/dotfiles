8db8ca82adcdd501ecdfd9b0056752044c139ee2
Rodrigo Cruz <labrugice@gmail.com>
2026-05-11 10:09:32 +0200

    Update mason

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -4,11 +4,19 @@ return {
   opts = {
     ensure_installed = {
       -- tools & formatters
+      "black",
       "blade-formatter",
       "eslint_d",
+      "isort",
+      "markdownlint-cli2",
+      "php-cs-fixer",
       "phpstan",
       "prettierd",
+      "rustywind",
+      "shfmt",
       "stylua",
+      "swiftformat",
+      "swiftlint",
       "pint",
 
       -- language servers (also OK here, but mainly installed via mason-lspconfig)
