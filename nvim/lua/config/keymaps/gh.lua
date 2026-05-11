ac776ff7383e632a6552110ffde53fee7b5f0656
Rodrigo Cruz <labrugice@gmail.com>
2026-05-11 10:31:13 +0200

    Improve github keybinds

--- nvim/lua/config/keymaps/gh.lua
+++ nvim/lua/config/keymaps/gh.lua
@@ -1,34 +1,41 @@
 local wk = require("which-key")
+
 wk.add({
   { "<leader>gH", group = "Github" },
   { "<leader>gHc", group = "Commits" },
-  { "<leader>gHcc", "<cmd>GHCloseCommit<cr>", desc = "Close" },
-  { "<leader>gHce", "<cmd>GHExpandCommit<cr>", desc = "Expand" },
-  { "<leader>gHco", "<cmd>GHOpenToCommit<cr>", desc = "Open To" },
-  { "<leader>gHcp", "<cmd>GHPopOutCommit<cr>", desc = "Pop Out" },
-  { "<leader>gHcz", "<cmd>GHCollapseCommit<cr>", desc = "Collapse" },
   { "<leader>gHi", group = "Issues" },
-  { "<leader>gHip", "<cmd>GHPreviewIssue<cr>", desc = "Preview" },
   { "<leader>gHl", group = "Litee" },
-  { "<leader>gHlt", "<cmd>LTPanel<cr>", desc = "Toggle Panel" },
   { "<leader>gHp", group = "Pull Request" },
-  { "<leader>gHpc", "<cmd>GHClosePR<cr>", desc = "Close" },
-  { "<leader>gHpd", "<cmd>GHPRDetails<cr>", desc = "Details" },
-  { "<leader>gHpe", "<cmd>GHExpandPR<cr>", desc = "Expand" },
-  { "<leader>gHpo", "<cmd>GHOpenPR<cr>", desc = "Open" },
-  { "<leader>gHpp", "<cmd>GHPopOutPR<cr>", desc = "PopOut" },
-  { "<leader>gHpr", "<cmd>GHRefreshPR<cr>", desc = "Refresh" },
-  { "<leader>gHpt", "<cmd>GHOpenToPR<cr>", desc = "Open To" },
-  { "<leader>gHpz", "<cmd>GHCollapsePR<cr>", desc = "Collapse" },
   { "<leader>gHr", group = "Review" },
-  { "<leader>gHrb", "<cmd>GHStartReview<cr>", desc = "Begin" },
-  { "<leader>gHrc", "<cmd>GHCloseReview<cr>", desc = "Close" },
-  { "<leader>gHrd", "<cmd>GHDeleteReview<cr>", desc = "Delete" },
-  { "<leader>gHre", "<cmd>GHExpandReview<cr>", desc = "Expand" },
-  { "<leader>gHrs", "<cmd>GHSubmitReview<cr>", desc = "Submit" },
-  { "<leader>gHrz", "<cmd>GHCollapseReview<cr>", desc = "Collapse" },
   { "<leader>gHt", group = "Threads" },
-  { "<leader>gHtc", "<cmd>GHCreateThread<cr>", desc = "Create" },
-  { "<leader>gHtn", "<cmd>GHNextThread<cr>", desc = "Next" },
-  { "<leader>gHtt", "<cmd>GHToggleThread<cr>", desc = "Toggle" },
 })
+
+vim.keymap.set("n", "<leader>gHcc", "<cmd>GHCloseCommit<cr>", { desc = "Close" })
+vim.keymap.set("n", "<leader>gHce", "<cmd>GHExpandCommit<cr>", { desc = "Expand" })
+vim.keymap.set("n", "<leader>gHco", "<cmd>GHOpenToCommit<cr>", { desc = "Open To" })
+vim.keymap.set("n", "<leader>gHcp", "<cmd>GHPopOutCommit<cr>", { desc = "Pop Out" })
+vim.keymap.set("n", "<leader>gHcz", "<cmd>GHCollapseCommit<cr>", { desc = "Collapse" })
+
+vim.keymap.set("n", "<leader>gHip", "<cmd>GHPreviewIssue<cr>", { desc = "Preview" })
+
+vim.keymap.set("n", "<leader>gHlt", "<cmd>LTPanel<cr>", { desc = "Toggle Panel" })
+
+vim.keymap.set("n", "<leader>gHpc", "<cmd>GHClosePR<cr>", { desc = "Close" })
+vim.keymap.set("n", "<leader>gHpd", "<cmd>GHPRDetails<cr>", { desc = "Details" })
+vim.keymap.set("n", "<leader>gHpe", "<cmd>GHExpandPR<cr>", { desc = "Expand" })
+vim.keymap.set("n", "<leader>gHpo", "<cmd>GHOpenPR<cr>", { desc = "Open" })
+vim.keymap.set("n", "<leader>gHpp", "<cmd>GHPopOutPR<cr>", { desc = "PopOut" })
+vim.keymap.set("n", "<leader>gHpr", "<cmd>GHRefreshPR<cr>", { desc = "Refresh" })
+vim.keymap.set("n", "<leader>gHpt", "<cmd>GHOpenToPR<cr>", { desc = "Open To" })
+vim.keymap.set("n", "<leader>gHpz", "<cmd>GHCollapsePR<cr>", { desc = "Collapse" })
+
+vim.keymap.set("n", "<leader>gHrb", "<cmd>GHStartReview<cr>", { desc = "Begin" })
+vim.keymap.set("n", "<leader>gHrc", "<cmd>GHCloseReview<cr>", { desc = "Close" })
+vim.keymap.set("n", "<leader>gHrd", "<cmd>GHDeleteReview<cr>", { desc = "Delete" })
+vim.keymap.set("n", "<leader>gHre", "<cmd>GHExpandReview<cr>", { desc = "Expand" })
+vim.keymap.set("n", "<leader>gHrs", "<cmd>GHSubmitReview<cr>", { desc = "Submit" })
+vim.keymap.set("n", "<leader>gHrz", "<cmd>GHCollapseReview<cr>", { desc = "Collapse" })
+
+vim.keymap.set("n", "<leader>gHtc", "<cmd>GHCreateThread<cr>", { desc = "Create" })
+vim.keymap.set("n", "<leader>gHtn", "<cmd>GHNextThread<cr>", { desc = "Next" })
+vim.keymap.set("n", "<leader>gHtt", "<cmd>GHToggleThread<cr>", { desc = "Toggle" })
